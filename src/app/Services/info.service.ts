dbe001fe76c37492a09a002bf8092ae3e4238eb2
Dylan Adekpe <dylanadekpe14@gmail.com>
2022-02-09 10:06:44 +0000

    second commit, modification du scss responsive

--- src/app/Services/info.service.ts
+++ src/app/Services/info.service.ts
@@ -8,40 +8,40 @@ export class InfoService {
   servTab = [
     {
       name: "Développement d'application web",
-      img: "url(service2.jpg)"
+      img: "url(assets/images/service2.jpg)"
 
     },
     {
       name: "Sauvegarde",
-      img: "url(./../../assets/images/data-center.jpg)"
+      img: "url(assets/images/data-center.jpg)"
     },
     {
       name: "Maintenace / Entretien",
-      img: "url(accueil3.jpg)"
+      img: "url(assets/images/accueil3.jpg)"
     },
     {
       name: "Gestion électronique des données",
-      img: "url(./../../assets/images/Icon.jpeg)"
+      img: "url(assets/images/Icon.jpeg)"
     },
     {
       name: "Solution mobile",
-      img: "url(./../../assets/images/programming.jpg)"
+      img: "url(assets/images/programming.jpg)"
     },
     {
       name: "Raccordement inter site",
-      img: "url(./../../assets/images/inter-site.jpg)"
+      img: "url(assets/images/inter-site.jpg)"
     },
     {
       name: "Installation réseau",
-      img: "url(./../../assets/images/service1.jpg)"
+      img: "url(assets/images/service1.jpg)"
     },
     {
       name: "Création de site web et messagerie professeionelle",
-      img: "url(./../../assets/images/bureau_2.jpg)"
+      img: "url(assets/images/bureau_2.jpg)"
     },
     {
       name: "Installation Camera de sécurité",
-      img: "url(./../../assets/images/cctv.jpg)"
+      img: "url(assets/images/cctv.jpg)"
     }
 
 
